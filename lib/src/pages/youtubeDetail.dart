839dcd1f4a76fc5a86a47fb1edb46778358c8055
sidongmen <192viva@gmail.com>
2021-03-21 19:34:23 +0900

    youtube detail page layout design

--- lib/src/pages/youtubeDetail.dart
+++ lib/src/pages/youtubeDetail.dart
@@ -1,14 +1,134 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 
 class YoutubeDetail extends StatelessWidget {
   const YoutubeDetail({Key key}) : super(key: key);
 
+  Widget _titleZone() {
+    return Container(
+        padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text("title 입니다 깔깔", style: TextStyle(fontSize: 15)),
+            Row(
+              children: [
+                Text(
+                  "조회수 1000회",
+                  style: TextStyle(
+                      fontSize: 12, color: Colors.black.withOpacity(0.6)),
+                ),
+                Text(" · "),
+                Text(
+                  "2021-03-21",
+                  style: TextStyle(
+                      fontSize: 12, color: Colors.black.withOpacity(0.6)),
+                ),
+              ],
+            )
+          ],
+        ));
+  }
+
+  Widget _descriptionZone() {
+    return Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+        child: Text(
+            "asdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdasdas",
+            style: TextStyle(fontSize: 14)));
+  }
+
+  Widget _buttonOne(String iconPath, String text) {
+    return Column(
+      children: [
+        SvgPicture.asset("assets/svg/icons/$iconPath.svg"),
+        Text(text)
+      ],
+    );
+  }
+
+  Widget _buttonZone() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        _buttonOne("like", "1000"),
+        _buttonOne("dislike", "0"),
+        _buttonOne("share", "공유"),
+        _buttonOne("save", "저장")
+      ],
+    );
+  }
+
+  Widget _ownerZone() {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+      child: Row(
+        children: [
+          CircleAvatar(
+            radius: 30,
+            backgroundColor: Colors.grey.withOpacity(0.5),
+            backgroundImage: Image.network(
+                    "https://post-phinf.pstatic.net/MjAxOTEyMDRfOSAg/MDAxNTc1NDI1MTg2MDE2.b1S1g-yhSiy6hxFJOoMsO7-PlMTc2iWAdznJ2xZwTxQg.3kTCo5pOPX6G3wtYR1AAYeGetDTkOXO2xTCM0SU4bNcg.JPEG/253-%EC%95%84%EC%9D%B4%EC%9C%A04.jpg?type=w1200")
+                .image,
+          ),
+          SizedBox(
+            width: 15,
+          ),
+          Expanded(
+              child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text("가나다라마바", style: TextStyle(fontSize: 18)),
+              Text("구독자 1000",
+                  style: TextStyle(
+                      fontSize: 14, color: Colors.black.withOpacity(0.6)))
+            ],
+          )),
+          GestureDetector(
+            child: Text(
+              "구독",
+              style: TextStyle(color: Colors.red, fontSize: 16),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _description() {
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          _titleZone(),
+          Divider(),
+          _descriptionZone(),
+          _buttonZone(),
+          SizedBox(
+            height: 20,
+          ),
+          Divider(),
+          _ownerZone()
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text(Get.parameters["videoId"]),
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: [
+          Container(
+            height: 250,
+            color: Colors.grey.withOpacity(0.5),
+          ),
+          Expanded(
+            child: _description(),
+          )
+        ],
       ),
     );
   }
